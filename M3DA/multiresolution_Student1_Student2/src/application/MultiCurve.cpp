3a4825b21f6c379811d26ea61b762cfb4d6ed3ee
Unknown <timothee.laval@gmail.com>
2017-12-11 23:38:54 +0100

    m3da tp4

--- M3DA/multiresolution_Student1_Student2/src/application/MultiCurve.cpp
+++ M3DA/multiresolution_Student1_Student2/src/application/MultiCurve.cpp
@@ -124,7 +124,9 @@ void MultiCurve::synthesisStep() {
    */
   finer.resize(n*2); // finer will contain next level
 
-
+  for(int i = 0; i < n/2; i++){
+  	  finer[i] = 
+  }
 
   /* end TODO
    */
@@ -140,12 +142,13 @@ void MultiCurve::analysisStep() {
   int level=log2(n)-1;
   vector<Vector3> coarse;
   coarse.resize(n/2); // coarse will contain the previous level
-
-
-  /* TODO : set the vector coarse and _detail[level] to represent the level curve from level+1
+    /* TODO : set the vector coarse and _detail[level] to represent the level curve from level+1
    * use _currentCurve (contains the points of the current level+1)
    */
-
+  for (int i = 0; i < n/2; i++) {
+        coarse[i] = 1/4 * (-1 * _currentCurve[(2*i) % n] + 3 * _currentCurve[(2*i+1) % n] + 3 * _currentCurve[(2*i+2) % n] - 1 * _currentCurve[(2*i+3) % n]);	
+        _detail[level][i] =1/4 * (1 * _currentCurve[(2*i) % n] - 3 * _currentCurve[(2*i+1) % n] + 3 * _currentCurve[(2*i+2) % n] - 1 * _currentCurve[(2*i+3) % n]);
+  }
 
 
   /* end TODO
